fdbe018e2aa3e5f1f3d4dc1b8f54202ddb377d3f
Jan Szczepan <janszczepan62@gmail.com>
2022-01-31 22:15:24 +0100

    Styling Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,11 @@
+import './Form.css';
 import React from 'react';
 
 const Form = () => {
    return (  
-      <form>
-         form
+      <form className='form'>
+         <input className='form__input' type="text" />
+         <button className='form__btn' type='submit'><i class="form__icon fas fa-search"></i></button>
       </form>
    );
 }

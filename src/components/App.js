179d50040eed1a42fe54ed6e3a9f93e5c1cc6524
Jan Szczepan <janszczepan62@gmail.com>
2022-01-31 20:58:48 +0100

    Basic structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Details from './Details';
+import Form from './Form';
+import Result from './Result';
 
 function App() {
   return (
-    <div className="App">
-      coś
+    <div className="container">
+      <main className="app">
+        <section className="app__result">
+          <Result />
+        </section>
+        <aside className="app__aside">
+          <Form />
+          <Details />
+        </aside>
+      </main>
     </div>
   );
 }

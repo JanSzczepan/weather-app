970f340a0a004aad072983cd08da2a06f4e74a63
Jan Szczepan <janszczepan62@gmail.com>
2022-02-01 20:38:41 +0100

    Control search input

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,35 @@
+import React, {Component} from 'react';
 import './App.css';
 import Details from './Details';
 import Form from './Form';
 import Result from './Result';
 
-function App() {
-  return (
-    <div className="container">
-      <main className="app">
-        <section className="app__result">
-          <Result />
-        </section>
-        <aside className="app__aside">
-          <Form />
-          <Details />
-        </aside>
-      </main>
-    </div>
-  );
+class App extends Component {
+  state = {
+    value: '',
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <main className="app">
+          <section className="app__result">
+            <Result />
+          </section>
+          <aside className="app__aside">
+            <Form value={this.state.value} change={this.handleInputChange}/>
+            <Details />
+          </aside>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,14 @@
 import './Form.css';
 import React from 'react';
 
-const Form = () => {
+const Form = props => {
    return (  
       <form className='form'>
-         <input className='form__input' type="text" />
+         <input 
+            className='form__input' 
+            type="text"
+            value={props.value}
+            onChange={props.change} />
          <button className='form__btn' type='submit'><i className="form__icon fas fa-search"></i></button>
       </form>
    );
